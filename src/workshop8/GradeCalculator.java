ff766352f72f186c09590870699ebc882da2a149
Pratik07007 <s.dhimal006@gmail.com>
2025-01-23 18:57:48 +0545

    feat:grades

--- src/workshop8/GradeCalculator.java
+++ src/workshop8/GradeCalculator.java
@@ -2,8 +2,14 @@ package workshop8;
 
 public class GradeCalculator {
 
+	public static float gradesCalc(float NMC, float AI) {
+		return NMC + AI;
+
+	}
+
 	public static void main(String[] args) {
-			// this calculates the grade for a student
+		float grade = gradesCalc(12, 123);
+		System.out.println(grade);
 
 	}
 
